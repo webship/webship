68b48ba88a03f0d1ca05b8b93aa07d54bf21acf0
ShaimaaNatshah <shaimaa.natshah11@gmail.com>
2022-05-21 21:20:33 +0300

    Issue #3281745: Add Webship JS to use the ready step definitions for automated functional testing in the Webship profil

--- tests/step-definitions/webship.js
+++ tests/step-definitions/webship.js
@@ -2,18 +2,6 @@ const {Given} = require('@cucumber/cucumber');
 const {When, Before} = require('@cucumber/cucumber');
 const {Then} = require('@cucumber/cucumber');
 
-Given(/^I go to "([^"]*)?"$/, function( url) {
-  return browser.url(url);
-});
-
-Then(/^I should( not)* see "([^"]*)?"$/, function(negativeCase, expectedText) {
-  if (negativeCase) {
-    return browser.assert.not.textContains('body', expectedText);
-  }
-  
-  return browser.assert.textContains('body', expectedText);
-});
-
 When(/^I navigate to "([^"]*)?"$/, function( url) {
   return browser.url(url);
   
