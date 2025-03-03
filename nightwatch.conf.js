572a471af04b79fd9468eeec62f436c0a5db0450
Tasneem Natshah <t.natshah@gmail.com>
2025-03-03 14:15:59 +0300

    Issue #3510470: Fix CircleCI

--- nightwatch.conf.js
+++ nightwatch.conf.js
@@ -22,7 +22,7 @@ module.exports = {
 
   test_settings: {
     default: {
-      launch_url: 'http://webship.test',
+      launch_url: 'http://webship.test/wd/hub',
       start_process: true,
       selenium_port: 4444,
       selenium_host: '127.0.0.1',
